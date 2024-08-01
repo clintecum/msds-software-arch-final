b9840ccdb6d736537b08593061cf7401f1141ca5
Chris Lintecum <chris@Chriss-MacBook-Pro.local>
2024-07-31 19:10:32 -0500

    switch to http server

--- server/index.js
+++ server/index.js
@@ -1,23 +1,27 @@
-const https = require("node:https");
-const fs = require("node:fs");
-const path = require('node:path');
+// const https = require("node:https");
+const http = require("node:http");
+// const fs = require("node:fs");
+// const path = require("node:path");
 const app = require("./app");
 const database = require("./lib/database");
 
 const port = process.env.PORT || 3000;
 
-const key = fs.readFileSync(path.join(__dirname, "certs", "server.key"));
-const cert = fs.readFileSync(path.join(__dirname, "certs", "server.crt"));
+// const key = fs.readFileSync(path.join(__dirname, "certs", "server.key"));
+// const cert = fs.readFileSync(path.join(__dirname, "certs", "server.crt"));
 
-const options = {
-  key: key,
-  cert: cert,
-  passphrase: 'yourpassword'
-};
+// const options = {
+//   key: key,
+//   cert: cert,
+//   passphrase: "yourpassword",
+// };
 
 const main = async () => {
   database.initialize();
-  https.createServer(options, app).listen(port, () => {
+  //   https.createServer(options, app).listen(port, () => {
+  //     console.log(`Server listening on port ${port}`);
+  //   });
+  http.createServer(app).listen(port, () => {
     console.log(`Server listening on port ${port}`);
   });
 };
